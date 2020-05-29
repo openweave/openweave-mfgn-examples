49bbdee26925c8f4ba54ae1647fc6bcb7377c67b
Pierre Delisle <pierred@google.com>
2020-05-29 13:45:16 -0700

    Ran clang-format and applied some formatting changes suggested to the md file (65 char/line will have to wait until we have formatting tool for that).

--- src/examples/lock/DeviceController.cpp
+++ src/examples/lock/DeviceController.cpp
@@ -441,7 +441,8 @@ void DeviceController::FactoryResetButtonHandler()
  *
  * Since the multicast must reach Sleepy End Devices, the multicast address is constructed following these guidelines:
  * - Sleep End Devices subscribe to the mesh local prefix-based multicast addresses (link local and realm local) by default.
- * - To reach the SED in the realm local, you can use the Realm-Local mesh local prefix-based multicast address which follows these rules according to RFC3306.
+ * - To reach the SED in the realm local, you can use the Realm-Local mesh local prefix-based multicast address which follows these
+ * rules according to RFC3306.
  *     - flags: 3
  *     - scope: 3
  *     - plen: mesh local prefix length
@@ -464,10 +465,11 @@ void DeviceController::SendIdentifyRequestButtonHandler()
 
     // Get the Thread mesh-local prefix
     const otMeshLocalPrefix * otMeshPrefix = otThreadGetMeshLocalPrefix(ThreadStackMgrImpl().OTInstance());
-    uint64_t otMeshPrefix64 = nl::Weave::Encoding::BigEndian::Get64(otMeshPrefix->m8);
+    uint64_t otMeshPrefix64                = nl::Weave::Encoding::BigEndian::Get64(otMeshPrefix->m8);
 
     // Construct the all-thread-nodes multicast address
-    nl::Inet::IPAddress allThreadNodesAddr = IPAddress::MakeIPv6PrefixMulticast(nl::Inet::kIPv6MulticastScope_Realm, 64, otMeshPrefix64, 1);
+    nl::Inet::IPAddress allThreadNodesAddr =
+        IPAddress::MakeIPv6PrefixMulticast(nl::Inet::kIPv6MulticastScope_Realm, 64, otMeshPrefix64, 1);
     char addrStr[50];
     allThreadNodesAddr.ToString(addrStr, sizeof(addrStr));
     WeaveLogDetail(Support, "All thread nodes multicast address: [%s]", addrStr);
@@ -481,10 +483,10 @@ void DeviceController::SendIdentifyRequestButtonHandler()
 
     WeaveLogProgress(Support, "Sending the Identify request");
     WeaveLogProgress(Support,
-            "fabric [0x%016" PRIx64 "] modes [0x%08" PRIx32 "] vendor [0x%04" PRIx16 "] product [0x%04" PRIx16 "] device [0x%016" PRIx64 "]",
-            identifyReqMsg.TargetFabricId, identifyReqMsg.TargetModes,
-            identifyReqMsg.TargetVendorId, identifyReqMsg.TargetProductId,
-            identifyReqMsg.TargetDeviceId);
+                     "fabric [0x%016" PRIx64 "] modes [0x%08" PRIx32 "] vendor [0x%04" PRIx16 "] product [0x%04" PRIx16
+                     "] device [0x%016" PRIx64 "]",
+                     identifyReqMsg.TargetFabricId, identifyReqMsg.TargetModes, identifyReqMsg.TargetVendorId,
+                     identifyReqMsg.TargetProductId, identifyReqMsg.TargetDeviceId);
     WEAVE_ERROR err = _this.mDeviceDescriptionClient.SendIdentifyRequest(allThreadNodesAddr, identifyReqMsg);
     if (err != WEAVE_NO_ERROR)
     {
@@ -497,7 +499,7 @@ void DeviceController::OnIdentifyResponseReceivedHandler(void * appState, uint64
                                                          const IdentifyResponseMessage & respMsg)
 {
     WeaveLogProgress(Support, "OnIdentifyResponseReceivedHandler");
-    DeviceController & _this         = GetDeviceController();
+    DeviceController & _this = GetDeviceController();
 
     WeaveDeviceDescriptor deviceDesc = respMsg.DeviceDesc;
     char ipAddrStr[64];
